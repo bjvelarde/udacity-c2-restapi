6621e8efbd83a43ea98b25f72845ed1cb6d6952f
Benjie Velarde <benjamin.velarde@disney.com>
2019-10-21 11:59:45 +0800

    ts dotenv to package.json

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import { config as dotenvConfig } from 'dotenv';
 if (process.env.DOTENV !== 'initialized' && process.env.AWS_PROFILE !== 'DEPLOYED') {
   // Load environment variables form `.env` file and merge with variables from environment
-  dotenv.config() || console.log(
+  dotenvConfig() || console.log(
       'Application is configured by env variables and default values.' +
       ' No .env configuration file was detected.');
   process.env.DOTENV = 'initialized';
